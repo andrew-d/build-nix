991ffbb95dd50e5737cebb2fed7c7ca24715909c
Andrew Dunham <andrew@du.nham.ca>
2018-05-23 01:08:12 -0700

    Get building on Linux

--- default.nix
+++ default.nix
@@ -56,13 +56,42 @@ in {
         ++ pkgs.lib.optional pkgs.stdenv.isLinux libseccomp
         ++ pkgs.lib.optionals (pkgs.stdenv.isLinux || pkgs.stdenv.isDarwin) [ aws-sdk-cpp libsodium ];
 
+      # TODO: conditional
+      prePatch = let
+        inherit (pkgs.lib) getLib;
+
+        in ''
+          find . -name local.mk -exec \
+            sed -Ei \
+              -e 's|-laws-cpp-sdk-([^ ]+)|${getLib staticPkgs.aws-sdk-cpp}/lib/libaws-cpp-sdk-\1.a|g' \
+              -e 's|-lbz2|${getLib staticPkgs.bzip2}/lib/libbz2.a|g' \
+              -e 's|-lseccomp|${getLib staticPkgs.libseccomp}/lib/libseccomp.a|g' \
+              -e 's|-l(boost_[^ ]+)|${getLib staticPkgs.boost}/lib/lib\1.a|g' \
+              {} \;
+        '';
+
       configureFlags =
         [ "--disable-init-state"
           "--enable-gc"
-          "--sysconfdir=/etc"
         ] ++ lib.optionals stdenv.isLinux [
           "--with-sandbox-shell=${pkgs.busybox-sandbox-shell}/bin/busybox"
         ];
+
+      preConfigure = let
+        inherit (pkgs.lib) getLib;
+
+        in ''
+          # Manually set library paths, since we may need spaces
+          configureFlagsArray=(
+            "BDW_GC_LIBS=${getLib staticPkgs.boehmgc}/lib/libgc.a"
+            "LIBBROTLI_LIBS=${getLib staticPkgs.brotli}/lib/libbrotlidec-static.a ${getLib staticPkgs.brotli}/lib/libbrotlienc-static.a ${staticPkgs.brotli.lib}/lib/libbrotlicommon-static.a"
+            "LIBCURL_LIBS=${getLib staticPkgs.curl}/lib/libcurl.a ${getLib staticPkgs.nghttp2}/lib/libnghttp2.a ${getLib staticPkgs.openssl}/lib/libssl.a ${getLib staticPkgs.openssl}/lib/libcrypto.a ${staticPkgs.zlib.static}/lib/libz.a"
+            "LIBLZMA_LIBS=${getLib staticPkgs.xz}/lib/liblzma.a"
+            ${if pkgs.stdenv.isLinux then "LIBSECCOMP_LIBS=${getLib staticPkgs.libseccomp}/lib/libseccomp.a" else ""}
+            ${if (pkgs.stdenv.isLinux || pkgs.stdenv.isDarwin) then "SODIUM_LIBS=${getLib staticPkgs.libsodium}/lib/libsodium.a" else ""}
+            "SQLITE3_LIBS=${getLib staticPkgs.sqlite}/lib/libsqlite3.a"
+          )
+        '';
 
       enableParallelBuilding = true;
 
@@ -70,16 +99,13 @@ in {
 
       makeFlags = [
         "BUILD_SHARED_LIBS=0"
-        "LDFLAGS=-static"
-        "GLOBAL_LDFLAGS=-static -lnghttp2 -lssl -lcrypto"
+        #"LDFLAGS=-static"
+        #"GLOBAL_LDFLAGS=-static -lnghttp2 -lssl -lcrypto"
+        "V=1"
       ];
 
       doCheck = false;
-
-      installFlags = "sysconfdir=$(out)/etc";
-
-      doInstallCheck = true;
-      installCheckFlags = "sysconfdir=$(out)/etc";
+      doInstallCheck = false;
     });
 
   # Lets us build individual dependencies
